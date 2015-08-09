29560ae97bce4a0bdcb5033df34d55026612cd37
Tommy Parnell <tparnell8@gmail.com>
2015-08-09 15:44:40 -0400

    remove headers

--- lmltfy/Global.asax.cs
+++ lmltfy/Global.asax.cs
@@ -25,11 +25,16 @@ namespace lmltfy
             //Access the db to JIT the EF dll, and generate a URL to JIT parts of our DLL (improve page loads on subsequent first time app starts)
 #if DEBUG
 #else
-            //MvcHandler.DisableMvcResponseHeader = true;
-           // Console.WriteLine(azuredb.GetModelByKey("tdszzxr3").Brand);
+            MvcHandler.DisableMvcResponseHeader = true;
+            Console.WriteLine(azuredb.GetModelByKey("tdszzxr3").Brand);
 
 #endif
         }
 
+        protected void Application_PreSendRequestHeaders(object sender, EventArgs e)
+        {
+            HttpContext.Current.Response.Headers.Remove("X-Powered-By");
+            HttpContext.Current.Response.Headers.Remove("X-AspNet-Version");
+        }
     }
 }
